24c265db84c693a55d51f9655580618b7a9a794a
Philipp Walter <philippwalter@proton.me>
2024-12-03 11:59:07 +0100

    fix: iOS imports

--- ios/SodiumReactNative.h
+++ ios/SodiumReactNative.h
@@ -1,12 +1,5 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNSodiumReactNativeSpec.h"
-
-@interface SodiumReactNative : NSObject <NativeSodiumReactNativeSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface SodiumReactNative : NSObject <RCTBridgeModule>
-#endif
 
 @end
